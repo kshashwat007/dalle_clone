76cb119da0710c5eaa50a4836c55914f71e3722f
kshashwat007 <kshashwat007@gmail.com>
2023-04-16 18:16:49 +0530

    Create page layout

--- client/types.d.ts
+++ client/types.d.ts
@@ -8,3 +8,14 @@ interface  IForm {
   prompt: string
   photo: string
 }
+
+interface IFormField {
+  labelName?: string,
+  type?: string,
+  name?: string,
+  placeholder?: string,
+  value?: string,
+  handleChange?: (e: React.FormEvent) => void,
+  isSurpriseMe?: boolean,
+  handleSurpriseMe?: () => void
+}
